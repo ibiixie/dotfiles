fb88b99777acaa19fea4752d7931874a2282f632
imxela <65206220+imxela@users.noreply.github.com>
2023-09-21 19:05:22 +0200

    Redo dotfiles

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,9 @@
+function fish_prompt
+    set -g __fish_get_prompt_showupstream auto
+
+    echo \
+    (set_color brmagenta)$USER@$HOSTNAME \
+    (set_color yellow)(prompt_pwd) \
+    (set_color red)(fish_vcs_prompt) \
+    (set_color cyan)"Ɛ> "
+end
