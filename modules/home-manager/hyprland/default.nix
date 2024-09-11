efe2ac44effb733178ac62081fe14713405f172f
imxela <65206220+imxela@users.noreply.github.com>
2024-09-11 11:53:54 +0200

    fix: wrong border color

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -25,7 +25,7 @@
 
 	  border_size = 2;
 
-          "col.active_border" = "rgba(ff7dd6ee) rgba(00ff99ee) 45deg";
+          "col.active_border" = "rgba(ff7dd6ee) rgba(ff69b4ee) 45deg";
 	  "col.inactive_border" = "rgba(595959aa)";
 
 	  resize_on_border = true;
