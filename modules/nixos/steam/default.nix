311505e1360b069103a0177de73f9a7fcc079737
imxela <65206220+imxela@users.noreply.github.com>
2024-11-25 19:10:12 +0100

    feat: fix steam stuff

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -8,15 +8,14 @@
 in {
   options = {
     modules.steam.enable = lib.mkEnableOption "enable Steam";
-    modules.steam.gamescope.enable = lib.mkEnableOption "enable Steam's gamescope compositor";
   };
 
   config = lib.mkIf config.modules.steam.enable {
     programs.steam = {
       enable = true;
 
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
+      # remotePlay.openFirewall = true;
+      # dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
 
       gamescopeSession.enable = true;
@@ -26,37 +25,5 @@ in {
       enable = true;
       capSysNice = true;
     };
-
-    hardware.xone.enable = true;
-    services.getty.autologinUser = "biixie";
-
-    environment = {
-      systemPackages = [
-        pkgs.mangohud
-      ];
-
-      loginShellInit = ''
-        [[ $(tty) = "/dev/tty1" ]] && ./gs.sh
-      '';
-    };
   };
-
-  # config = lib.mkIf config.modules.steam.gamescope.enable {
-  #   programs.steam.gamescopeSession.enable = true;
-
-  #   programs.gamescope = {
-  #     enable = true;
-  #     capSysNice = true;
-  #   };
-
-  #   hardware.xone.enable = true;
-  #   services.getty.autoLoginUser = "biixie";
-
-  #   environment = {
-  #     systemPackages = pkgs.mangohud;
-  #     loginShellInit = ''
-  # 	[[ $(tty) = "/dev/tty1" ]] && ./gs.sh
-  #     '';
-  #   };
-  # };
 }
