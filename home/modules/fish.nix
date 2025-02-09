ac547ad1d6c7d1267fa15c407fdac55bf87b5931
ibiixie <65206220+ibiixie@users.noreply.github.com>
2025-02-09 12:21:20 +0100

    wip: add a couple of fish abbrs and aliases

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -4,6 +4,24 @@
   programs.fish = {
     enable = true;
 
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
+
+      # sometimes you have to ask kindly c:
+      pls = "sudo";
+      please = "sudo";
+    };
+
+    shellAbbrs = {
+      # Git.
+      ga = "git add";
+      gc = "git commit -m";
+      gp = "git push";
+    };
+
     functions = {
       fish_greeting = ''
         # Keep it empty for now! 
