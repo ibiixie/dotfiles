e77f7e0e221d7c68742ef5f14ea2933f21989ce8
ibiixie <65206220+ibiixie@users.noreply.github.com>
2025-02-19 11:50:17 +0100

    feat(ssh): add ssh keys for oci

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -13,6 +13,12 @@
         user = "git";
         identityFile = "${config.sops.secrets."ssh/personal".path}";
       };
+
+      "oci-aux" = {
+        hostname = "aux.ssh.biixie.com";
+        user = "ubuntu";
+        identityFile = "${config.sops.secrets."ssh/oracle-vps".path}";
+      };
     };
   };
 }
