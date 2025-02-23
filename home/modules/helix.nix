fa4a6cd1e958a0658d77f8d64644d1a102ca717b
ibiixie <65206220+ibiixie@users.noreply.github.com>
2025-02-23 14:39:42 +0100

    feat(helix): configure language support for some more languages

--- home/modules/helix.nix
+++ home/modules/helix.nix
@@ -1,7 +1,11 @@
 {
+  pkgs,
   ...
 }:
 
+let
+  typescript = pkgs.typescript;
+in
 {
   programs.helix = {
     enable = true;
@@ -62,7 +66,126 @@
             ];
           };
         }
+
+        {
+          name = "rust";
+          auto-format = true;
+        }
+
+        {
+          name = "toml";
+          auto-format = true;
+        }
+
+        {
+          name = "dockerfile";
+          auto-format = true;
+        }
+
+        {
+          name = "docker-compose";
+          auto-format = true;
+        }
+
+        {
+          name = "json";
+          auto-format = true;
+        }
+
+        {
+          name = "jsonc";
+          auto-format = true;
+        }
+
+        {
+          name = "javascript";
+          auto-format = true;
+        }
+
+        {
+          name = "jsx";
+          auto-format = true;
+        }
+
+        {
+          name = "gjs";
+          auto-format = true;
+        }
+
+        {
+          name = "typescript";
+          auto-format = true;
+        }
+
+        {
+          name = "tsx";
+          auto-format = true;
+        }
+
+        {
+          name = "css";
+          auto-format = true;
+        }
+
+        {
+          name = "scss";
+          auto-format = true;
+        }
+
+        {
+          name = "html";
+          auto-format = true;
+        }
+
+        {
+          name = "gts";
+          auto-format = true;
+        }
+
+        {
+          name = "svelte";
+          auto-format = true;
+        }
+
+        {
+          name = "astro";
+          auto-format = true;
+          language-servers = [ "astro-ls" ];
+          # formatter = {
+          #   command = "prettier";
+          #   args = [
+          #     "--plugin"
+          #     "prettier-plugin-astro"
+          #     "--parser"
+          #     "astro"
+          #   ];
+          # };
+        }
       ];
+      language-server.rust-analyzer.config.check = {
+        command = "clippy";
+        # make clippy yell at me lots :D
+        extraArgs = [
+          "--"
+          "-W"
+          "clippy::unwrap_used"
+          "-W"
+          "clippy::expect_used"
+          "-W"
+          "clippy::pedantic"
+          "-W"
+          "clippy::nursery"
+        ];
+      };
+      language-server.astro-ls = {
+        command = "astro-ls";
+        args = [ "--stdio" ];
+        config = {
+          typescript = {
+            tsdk = "${typescript}/lib/node_modules/typescript/lib";
+          };
+        };
+      };
     };
   };
 }
