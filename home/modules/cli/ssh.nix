a45ff1249e8873eca3740a9dd0e5e79c5178abf1
biixie <no-reply@biixie.com>
2025-03-28 12:27:07 +0100

    feat(ssh): add host

--- home/modules/cli/ssh.nix
+++ home/modules/cli/ssh.nix
@@ -19,6 +19,12 @@
         user = "ubuntu";
         identityFile = "${config.sops.secrets."ssh/oracle-vps".path}";
       };
+
+      "gcp-aux" = {
+        hostname = "aux-gcp.ssh.biixie.com";
+        user = "${config.sops.secrets."ssh/gcp-vps-user".path}";
+        "identityFile" = "${config.sops.secrets."ssh/oracle-vps".path}";
+      };
     };
   };
 }
