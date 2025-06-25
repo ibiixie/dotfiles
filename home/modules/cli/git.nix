6c10b9c4ed5b065b9403405dc9a72d0615b53d04
biixie <no-reply@biixie.com>
2025-06-25 12:07:44 +0200

    feat(git): improve git config, add fish aliases for git

--- home/modules/cli/git.nix
+++ home/modules/cli/git.nix
@@ -10,7 +10,7 @@
     lfs.enable = true;
 
     userEmail = "65206220+ibiixie@users.noreply.github.com";
-    userName = "ibiixie";
+    userName = "biixie";
 
     signing.signByDefault = true;
     signing.key = "${config.sops.secrets."ssh/personal".path}";
@@ -18,6 +18,39 @@
     extraConfig = {
       gpg.format = "ssh";
       init.defaultBranch = "main";
+
+      core.whitespace = "error";
+      core.preloadindex = true;
+
+      status = {
+        branch = true;
+        showStash = true;
+        showUntrackedFiles = "all";
+      };
+
+      interactive = {
+        singlekey = true;
+      };
+
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
+
+      pull = {
+        default = "current";
+        rebase = true;
+      };
+
+      rebase = {
+        autoStash = true;
+        missingCommitsCheck = "warn";
+      };
+
+      log = {
+        abbrevCommit = true;
+        graphColors = "blue,yellow,cyan,magenta,green,red";
+      };
     };
   };
 }
