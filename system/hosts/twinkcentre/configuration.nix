99db2d59601b91a2451c40b6c01b3d69979b2429
biixie <no-reply@biixie.com>
2025-12-09 11:05:50 +0100

    fix(host/twinkcentre): fix typos in runner config, fix PAT permissions

--- system/hosts/twinkcentre/configuration.nix
+++ system/hosts/twinkcentre/configuration.nix
@@ -57,13 +57,13 @@
     };
 
     github-runners = {
-      twinkcentre-runner = {i
+      twinkcentre-runner = {
         enable = true;
         name = "twinkcentre-runner";
         url = "https://github.com/ibiixie/dedicated-infra";
         tokenFile = config.sops.secrets."hosts/twinkcentre/gh-runner-token".path;
-      }
-    }
+      };
+    };
   };
 
   # This value determines the NixOS release from which the default
