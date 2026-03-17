34404b3f06d4a87a266011af52798a56a3586457
biixie <no-reply@biixie.com>
2026-03-17 17:59:31 +0100

    fix(jay): fix screensharing
    portals and session weirdness fixed, hopefully!

--- system/modules/jay.nix
+++ system/modules/jay.nix
@@ -8,6 +8,29 @@
   # Needed for some GNOME apps (like easyeffects).
   programs.dconf.enable = true;
 
+  programs.xwayland.enable = true;
+
+  # required when using home-manager with userUserPackages enabled
+  # https://mynixos.com/home-manager/option/xdg.portal.enable
+  environment.pathsToLink = [
+    "/share/xdg-desktop-portal"
+    "/share/applications"
+  ];
+
+  xdg.portal = {
+    enable = true;
+    configPackages = [
+      pkgs.jay
+    ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  services.displayManager.sessionPackages = [
+    pkgs.jay
+  ];
+
   programs.uwsm = {
     enable = true;
 
