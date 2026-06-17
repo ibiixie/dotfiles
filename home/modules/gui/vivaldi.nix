19e49509e050cf28ebf108ec3dfdfd48ce10c558
biixie <no-reply@biixie.com>
2026-06-18 01:03:21 +0200

    feat(vivaldi): set vivaldi as default browser

--- home/modules/gui/vivaldi.nix
+++ home/modules/gui/vivaldi.nix
@@ -4,6 +4,20 @@
 }:
 
 {
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "vivaldi-stable.desktop";
+      "x-scheme-handler/http" = "vivaldi-stable.desktop";
+      "x-scheme-handler/https" = "vivaldi-stable.desktop";
+      "x-scheme-handler/about" = "vivaldi-stable.desktop";
+      "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+    };
+  };
+
+  home.sessionVariables.DEFAULT_BROWSER = "${pkgs.vivaldi}/bin/vivaldi";
+
   programs.chromium = {
     enable = true;
 
